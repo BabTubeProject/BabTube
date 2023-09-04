80a1ff72ccb750f9308a477a0d794c6ab2dec72e
dohyun <thtree523@gmail.com>
2023-09-04 16:55:48 +0900

    feat: RootViewController를 LoginViewController로 변경

--- BabTube/BabTube/LoginViewController/LoginViewController.swift
+++ BabTube/BabTube/LoginViewController/LoginViewController.swift
@@ -8,11 +8,28 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    let loginButton: UIButton = UIButton()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.addSubview(loginButton)
+        configButton()
         // Do any additional setup after loading the view.
+    }
+    
+    private func configButton() {
+        loginButton.setTitle("로그인", for: .normal)
+        loginButton.backgroundColor = .systemGray5
+        loginButton.frame.size = CGSize(width: 200, height: 100)
+        loginButton.center = view.center
+        loginButton.addTarget(self, action: #selector(loginButtonClick), for: .touchUpInside)
+    }
+    
+    @objc private func loginButtonClick() {
+        let vc = TabBarController()
+        vc.modalPresentationStyle = .fullScreen
+        present(vc, animated: false)
     }
     
 
