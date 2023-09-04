86f491b9ab7d981a77fd84dad4a46f2dfe372528
dohyun <thtree523@gmail.com>
2023-09-04 16:17:48 +0900

    project init

---
+++ BabTube/BabTube/Util/Color.swift
@@ -0,0 +1,12 @@
+//
+//  Color.swift
+//  BabTube
+//
+//  Created by 김도현 on 2023/09/04.
+//
+
+import UIKit
+
+extension UIColor {
+    static let mainColor: UIColor = .red
+}
---
+++ BabTube/BabTube/Util/Font.swift
@@ -0,0 +1,15 @@
+//
+//  Font.swift
+//  BabTube
+//
+//  Created by 김도현 on 2023/09/04.
+//
+
+import UIKit
+
+extension UIFont {
+    static let title1 = UIFont.systemFont(ofSize: 30, weight: .bold)
+    static let title2 = UIFont.systemFont(ofSize: 22, weight: .bold)
+    static let title3 = UIFont.systemFont(ofSize: 20, weight: .regular)
+    static let body = UIFont.systemFont(ofSize: 16, weight: .light)
+}
