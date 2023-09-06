c4481e1d3a5e4d30b60645b57247e1e606d880e4
dohyun <thtree523@gmail.com>
2023-09-06 12:06:20 +0900

    fix: 시작화면 수정

--- BabTube/BabTube/SceneDelegate.swift
+++ BabTube/BabTube/SceneDelegate.swift
@@ -16,8 +16,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let scene = (scene as? UIWindowScene) else { return }
         let window = UIWindow(windowScene: scene)
-//        let mainVC = UINavigationController(rootViewController: LoginViewController())
-        let mainVC = VideoDetailViewController()
+        let mainVC = UINavigationController(rootViewController: LoginViewController())
         window.backgroundColor = .white
         window.rootViewController = mainVC
         window.makeKeyAndVisible()
