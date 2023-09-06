006f08988a23b874217404193dfb9ef02a71fdfe
junyoung_hong <wnsdud0721@naver.com>
2023-09-06 09:52:14 +0900

    [REFACTOR] MainColor 변경 & MyPage 로고 크기 변경, TableView 스크롤 없애기

--- BabTube/BabTube/Util/Color.swift
+++ BabTube/BabTube/Util/Color.swift
@@ -8,5 +8,7 @@
 import UIKit
 
 extension UIColor {
-    static let mainColor: UIColor = .red
+    static let mainColor = UIColor(
+        red: 236/255, green: 33/255, blue: 35/255, alpha: 1
+    )
 }
